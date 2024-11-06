31317413707728defe24a82eccbd95be3180b14b
nouman <nauman@aussourceenterprises.com>
2024-11-06 10:53:38 +0500

    frontend CRUD done

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useProductStore } from "../../store/product";
+
+function Product({ product }) {
+  const { deleteProduct, updateProduct } = useProductStore();
+  const [name, setName] = useState(product?.name);
+
+  const handleUpdate = () => {
+    if (name !== product?.name)
+      updateProduct(product?._id, { ...product, name });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{
+          border: "none",
+          height: "45px",
+          borderRadius: "6px",
+        }}
+      />
+      <div>
+        <button
+          style={{ height: "50px" }}
+          onClick={() => deleteProduct(product?._id)}
+        >
+          delete
+        </button>
+        <button style={{ height: "50px" }} onClick={handleUpdate}>
+          update
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useProductStore } from "../../store/product";
+import Product from "./Product";
 
 function Products() {
-  const { products, deleteProduct, fetchProducts } = useProductStore();
+  const { products, fetchProducts } = useProductStore();
 
   useEffect(() => {
     fetchProducts();
@@ -12,22 +13,7 @@ function Products() {
     <div style={{ userSelect: "none", width: "350px", marginBottom: "24px" }}>
       {products.length > 0 &&
         products.map((product) => (
-          <div
-            key={product._id}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <p>{product?.name}</p>
-            <button
-              style={{ height: "50px" }}
-              onClick={() => deleteProduct(product?._id)}
-            >
-              delete
-            </button>
-          </div>
+          <Product key={product._id} product={product} />
         ))}
     </div>
   );
