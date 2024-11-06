b8fe1e86198b8385ebe31cb1ac6a1b151ba1afeb
nouman <nauman@aussourceenterprises.com>
2024-11-06 11:35:20 +0500

    deployment configured

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve(); // root directory location
+
 app.use(express.json()); // allows to use JSON data in the req.body
 
 app.use("/api/products", productRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
