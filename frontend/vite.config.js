1c728c37409a576cef921f16d594424a509ad92f
nouman <nauman@aussourceenterprises.com>
2024-11-07 12:44:09 +0500

    updated deployment configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://mern-crud-backend-liard.vercel.app/",
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:5000",
+  //     },
+  //   },
+  // },
 });
